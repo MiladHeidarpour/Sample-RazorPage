7416a42182c7a3854a313cdd81c0f80a4957d6ad
Milad HeidarPour <mld.heidarpour@gmail.com>
2024-08-22 10:40:44 +0330

    SE04 => BannerService

--- Shop.RazorPage/Services/Banners/IBannerService.cs
+++ Shop.RazorPage/Services/Banners/IBannerService.cs
@@ -1,5 +1,15 @@
-﻿namespace Shop.RazorPage.Services.Banners;
+﻿using Shop.RazorPage.Models;
+using Shop.RazorPage.Models.Command.Banners;
+using Shop.RazorPage.Models.Response.Banners;
+
+namespace Shop.RazorPage.Services.Banners;
 
 public interface IBannerService
 {
+    Task<ApiResult> CreateBanner(CreateBannerCommand command);
+    Task<ApiResult> EditBanner(EditBannerCommand command);
+    Task<ApiResult> DeleteBanner(long bannerId);
+
+    Task<BannerDto?> GetBannerById(long bannerId);
+    Task<List<BannerDto>> GetList();
 }
